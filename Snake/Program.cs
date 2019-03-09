cb261bcd21dcc6a82e1a54997ab42b974d4161fc
Maksim Kovalenko <norbe3t@gmail.com>
2019-03-09 20:59:14 +0200

    Code review

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Collections.Generic;
 
 namespace Snake
@@ -15,71 +16,34 @@ namespace Snake
 
     class SnakeGame
     {
-        public void Menu(){}
+        public void Menu() { }
         public void Start()
         {
-            var _gameField = new GameField(100, 20);
-            var _food = new Food(5, 5);
-            var _snakeTail = new List<SnakeTail>();
-            var _snake = new Snake(_gameField, _snakeTail, 10, 10);
-            var _draw = new Draw(_gameField, _snake, _snakeTail, _food);
+            var _gameField = new GameField(30, 20);
+            var _draw = new Draw(_gameField);
 
             _draw.DrawArea();
             _draw.DrawSnake();
             _draw.DrawFood();
 
-            ConsoleKeyInfo keyinfo;
-            do
+            var _snakeControl = new SnakeControl(_draw);
+
+            while (true)
             {
-                keyinfo = Console.ReadKey();
-                switch (keyinfo.Key)
-                {
-                    case ConsoleKey.UpArrow:
-                        _snake.action.MoveUp();
-                        if(_snake.action.TryEat(_snake ,ref _snakeTail, ref _food))
-                        {
-                            _draw.DrawFood();
-                        }
-                        _draw.DrawSnake();
-                        _draw.DrawSnakeTail();
-                        break;
-
-                    case ConsoleKey.DownArrow:
-                        _snake.action.MoveDown();
-                        if(_snake.action.TryEat(_snake ,ref _snakeTail, ref _food))
-                        {
-                            _draw.DrawFood();
-                        }
-                        _draw.DrawSnake();
-                        _draw.DrawSnakeTail();
-                        break;
-
-                    case ConsoleKey.LeftArrow:
-                        _snake.action.MoveLeft();
-                        if(_snake.action.TryEat(_snake ,ref _snakeTail, ref _food))
-                        {
-                            _draw.DrawFood();
-                        }
-                        _draw.DrawSnake();
-                        _draw.DrawSnakeTail();
-                        break;
-
-                    case ConsoleKey.RightArrow:
-                        _snake.action.MoveRight();
-                        if(_snake.action.TryEat(_snake ,ref _snakeTail, ref _food))
-                        {
-                            _draw.DrawFood();
-                        }
-                        _draw.DrawSnake();
-                        _draw.DrawSnakeTail();
-                        break;
-                }
+                if(_snakeControl.StartMove())
+                    break;
+                Thread.Sleep(200);
             }
-            while (keyinfo.Key != ConsoleKey.X);
+            GameOver();
         }
-        public void Stop(){}
-        public void Restart(){}
-        public void Pause(){}
+        public void GameOver() 
+        { 
+            Console.Clear();
+            Console.WriteLine("Game Over !!!");
+            Console.ReadKey();
+        }
+        public void Restart() { }
+        public void Pause() { }
     }
 
     class GameField
@@ -97,7 +61,7 @@ namespace Snake
     class Draw
     {
         private GameField _gameField;
-        private Snake _snake;
+        public Snake _snake { get; private set; }
         private List<SnakeTail> _snakeTail;
         private Food _food;
 
@@ -106,12 +70,12 @@ namespace Snake
         public int snakeTailX { get; set; }
         public int snakeTailY { get; set; }
 
-        public Draw(GameField gameField, Snake snake, List<SnakeTail> snakeTail, Food food)
+        public Draw(GameField gameField)
         {
             this._gameField = gameField;
-            this._snake = snake;
-            this._snakeTail = snakeTail;
-            this._food = food;
+            this._snakeTail = new List<SnakeTail>();
+            this._food = new Food(_gameField);
+            this._snake = new Snake(this, _gameField, _snakeTail, _food);
         }
 
         private void SetCursor()
@@ -148,18 +112,18 @@ namespace Snake
                     }
                     else
                     {
-                        Console.Write(".");
+                        Console.Write(" ");
                     }
                 }
             }
         }
 
-        public void DrawSnake()
+        public void DrawSnake(bool isSnakeTail = false)
         {
-            if (snakeX != 0 || snakeY != 0)
+            if (!isSnakeTail && (snakeX != 0 || snakeY != 0))
             {
                 SetCursor(snakeX, snakeY);
-                Console.Write(".");
+                Console.Write(" ");
             }
 
             SetCursor(_snake.X, _snake.Y);
@@ -179,7 +143,7 @@ namespace Snake
             if (snakeTailX != 0 || snakeTailY != 0)
             {
                 SetCursor(snakeTailX, snakeTailY);
-                Console.Write(".");
+                Console.Write(" ");
             }
             foreach (var item in _snakeTail)
             {
@@ -211,17 +175,23 @@ namespace Snake
             SetCursor();
         }
     }
-    
+
     class Snake
     {
+        private Random random;
+
         public int X { get; set; }
         public int Y { get; set; }
+        public List<SnakeTail> SnakeTail { get; private set; }
         public Action action;
-        public Snake(GameField gameField, List<SnakeTail> snakeTail, int x, int y)
+
+        public Snake(Draw draw, GameField gameField, List<SnakeTail> snakeTail, Food food)
         {
-            this.X = x;
-            this.Y = y;
-            action = new Action(this, snakeTail, gameField);
+            random = new Random();
+            this.X = random.Next(1, gameField.Width / 2);
+            this.Y = random.Next(1, gameField.Height / 2);
+            this.SnakeTail = snakeTail;
+            action = new Action(draw, this, food, gameField);
         }
     }
 
@@ -239,22 +209,28 @@ namespace Snake
 
     class Food
     {
+        private Random random;
         public int X { get; set; }
         public int Y { get; set; }
 
-        public Food(int x, int y)
+        public Food(GameField gameField)
         {
-            this.X = x;
-            this.Y = y;
+            random = new Random();
+            this.X = random.Next(gameField.Width / 2, gameField.Width - 2);
+            this.Y = random.Next(1, gameField.Height - 2);
         }
     }
 
     class Action
     {
+        private GameField _gameField;
+        private Draw _draw;
         private Snake _snake;
         private List<SnakeTail> _snakeTail;
-        private GameField _gameField;
+        private Food _food;
         private Random random = new Random();
+
+        private bool _isSnakeEatFood = false;
 
         int tempX = 0;
         int tempY = 0;
@@ -262,122 +238,107 @@ namespace Snake
         int tempXX = 0;
         int tempYY = 0;
 
-        public Action(Snake snake, List<SnakeTail> snakeTail, GameField gameField)
+        public Action(Draw draw, Snake snake, Food food, GameField gameField)
         {
+            this._draw = draw;
             this._snake = snake;
-            this._snakeTail = snakeTail;
+            this._snakeTail = _snake.SnakeTail;
+            this._food = food;
             this._gameField = gameField;
         }
 
-        public bool TryEat(Snake snake, ref List<SnakeTail> snakeTail, ref Food food)
+        public bool TryEat()
         {
-            if ((food.X == _snake.X) && (food.Y == _snake.Y))
+            if ((_food.X == _snake.X) && (_food.Y == _snake.Y))
             {
-                if (snakeTail.Count != 0)
+                if (_snakeTail.Count != 0)
                 {
-                    var lastSnakeTail = snakeTail.Last();
-                    snakeTail.Add(new SnakeTail(lastSnakeTail.X, lastSnakeTail.Y));
+                    var lastSnakeTail = _snakeTail.Last();
+                    _snakeTail.Add(new SnakeTail(lastSnakeTail.X, lastSnakeTail.Y));
                 }
                 else
                 {
-                    snakeTail.Add(new SnakeTail(snake.X, snake.Y));
+                    _snakeTail.Add(new SnakeTail(_snake.X, _snake.Y));
                 }
-                food.X = random.Next(1, _gameField.Width - 1);
-                food.Y = random.Next(1, _gameField.Height - 1);
+                _food.X = random.Next(1, _gameField.Width - 1);
+                _food.Y = random.Next(1, _gameField.Height - 1);
                 return true;
             }
             return false;
         }
-        public void MoveUp()
+        public bool MoveUp()
         {
-            for (int i = 0; i < _snakeTail.Count; i++)
-            {
-                
-                if(i == 0)
-                {
-                    tempX = _snakeTail[i].X;
-                    tempY = _snakeTail[i].Y;
-                    _snakeTail[i].X = _snake.X;
-                    _snakeTail[i].Y = _snake.Y;
-                }
-                else
-                {
-                    tempXX = tempX;
-                    tempYY = tempY;
-
-                    tempX = _snakeTail[i].X;
-                    tempY = _snakeTail[i].Y;
-                    _snakeTail[i].X = tempXX;
-                    _snakeTail[i].Y = tempYY; 
-                }
-            }
+            SnakeTail();
             if ((--_snake.Y) < 1)
             {
                 _snake.Y = _gameField.Height - 2;
             }
+            Draw(TryEat());
+            return CheckTail();
         }
-        public void MoveDown()
+        public bool MoveDown()
         {
-            for (int i = 0; i < _snakeTail.Count; i++)
-            {
-                
-                if(i == 0)
-                {
-                    tempX = _snakeTail[i].X;
-                    tempY = _snakeTail[i].Y;
-                    _snakeTail[i].X = _snake.X;
-                    _snakeTail[i].Y = _snake.Y;
-                }
-                else
-                {
-                    tempXX = tempX;
-                    tempYY = tempY;
-
-                    tempX = _snakeTail[i].X;
-                    tempY = _snakeTail[i].Y;
-                    _snakeTail[i].X = tempXX;
-                    _snakeTail[i].Y = tempYY; 
-                }
-            }
+            SnakeTail();
             if ((++_snake.Y) > _gameField.Height - 2)
             {
                 _snake.Y = 1;
             }
+            Draw(TryEat());
+            return CheckTail();
         }
-        public void MoveLeft()
+        public bool MoveLeft()
         {
-            for (int i = 0; i < _snakeTail.Count; i++)
-            {
-                
-                if(i == 0)
-                {
-                    tempX = _snakeTail[i].X;
-                    tempY = _snakeTail[i].Y;
-                    _snakeTail[i].X = _snake.X;
-                    _snakeTail[i].Y = _snake.Y;
-                }
-                else
-                {
-                    tempXX = tempX;
-                    tempYY = tempY;
-
-                    tempX = _snakeTail[i].X;
-                    tempY = _snakeTail[i].Y;
-                    _snakeTail[i].X = tempXX;
-                    _snakeTail[i].Y = tempYY; 
-                }
-            }
+            SnakeTail();
             if ((--_snake.X) < 1)
             {
                 _snake.X = _gameField.Width - 2;
             }
+            Draw(TryEat());
+            return CheckTail();
         }
-        public void MoveRight()
+        public bool MoveRight()
         {
-           for (int i = 0; i < _snakeTail.Count; i++)
+            SnakeTail();
+            if ((++_snake.X) > _gameField.Width - 2)
             {
-                
-                if(i == 0)
+                _snake.X = 1;
+            }
+            Draw(TryEat());
+            return CheckTail();
+        }
+
+        public void Draw(bool drawFood)
+        {
+            _draw.DrawSnakeTail();
+            _draw.DrawSnake(_isSnakeEatFood);
+            if (drawFood)
+            {
+                _draw.DrawFood();
+                _isSnakeEatFood = true;
+            }
+        }
+
+        public bool CheckTail()
+        {
+            if(_snakeTail.Count > 3)
+            {
+                foreach(var item in _snakeTail)
+                {
+                    if (_snake.X == item.X && _snake.Y == item.Y)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void SnakeTail()
+        {
+            for (int i = 0; i < _snakeTail.Count; i++)
+            {
+
+                if (i == 0)
                 {
                     tempX = _snakeTail[i].X;
                     tempY = _snakeTail[i].Y;
@@ -392,13 +353,71 @@ namespace Snake
                     tempX = _snakeTail[i].X;
                     tempY = _snakeTail[i].Y;
                     _snakeTail[i].X = tempXX;
-                    _snakeTail[i].Y = tempYY; 
+                    _snakeTail[i].Y = tempYY;
                 }
             }
-            if ((++_snake.X) > _gameField.Width - 2)
+        }
+    }
+
+    class SnakeControl
+    {
+        private Draw _draw;
+        private int _motion = 0;
+
+        public SnakeControl(Draw draw)
+        {
+            this._draw = draw;
+        }
+
+        public bool StartMove()
+        {
+            ConsoleKeyInfo keyinfo;
+            while (Console.KeyAvailable)
             {
-                _snake.X = 1;
+                keyinfo = Console.ReadKey();
+                switch (keyinfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (_motion != 1)
+                            _motion = 3;
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (_motion != 3)
+                            _motion = 1;
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        if (_motion != 0)
+                            _motion = 2;
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        if (_motion != 2)
+                            _motion = 0;
+                        break;
+                }
             }
+            return Move();
+        }
+
+        private bool Move()
+        {
+            switch (_motion)
+            {
+                case 0:
+                    return _draw._snake.action.MoveRight();
+
+                case 1:
+                    return _draw._snake.action.MoveDown();
+
+                case 2:
+                    return _draw._snake.action.MoveLeft();
+
+                case 3:
+                    return _draw._snake.action.MoveUp();
+            }
+            return false;
         }
     }
 }
